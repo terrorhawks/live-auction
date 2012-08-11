9ffe13832dbcb10a726b45884f81bb97d6a5baaa
David Clarke <terrorhawks@gmail.com>
2012-08-11 14:00:40 +1000

    deleted

--- deploy_android.rb
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/Library/Application\ Support/Titanium/mobilesdk/osx/__MACOSX/android/builder.py install rea-live-auction /Users/davidclarke/bin/android-sdk /Users/davidclarke/workspace/_titanium/live-auction HR4LVL7TKY.au.com.dius.liveauction 7
